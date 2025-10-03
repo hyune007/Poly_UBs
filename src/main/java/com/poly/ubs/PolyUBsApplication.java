f248f2867cb4d45cb9646ac45be2bfe06cc7ffcb
HyuMorreti <huyhuyhuy201107@gmail.com>
2025-10-03 18:00:49 +0700

    huynt - First commit

---
+++ src/main/java/com/poly/ubs/PolyUBsApplication.java
@@ -0,0 +1,13 @@
+package com.poly.ubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyUBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyUBsApplication.class, args);
+	}
+
+}
