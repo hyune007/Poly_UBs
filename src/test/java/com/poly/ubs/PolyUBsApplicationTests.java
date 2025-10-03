f318c13c4a67d5c227162a21884c561196fbc303
HyuMorreti <huyhuyhuy201107@gmail.com>
2025-10-03 18:00:49 +0700

    huynt - First commit

---
+++ src/test/java/com/poly/ubs/PolyUBsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.ubs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyUBsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
