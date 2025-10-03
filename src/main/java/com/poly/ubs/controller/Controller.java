8a57118fd3bb590898df71425482ff9003950bb2
kohacoder <172766095+kohacoder@users.noreply.github.com>
2025-10-04 00:39:28 +0700

    giao dien

---
+++ src/main/java/com/poly/ubs/controller/Controller.java
@@ -0,0 +1,31 @@
+package com.poly.ubs.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @RequestMapping("header")
+    public String index(Model model) {
+        return "/main-frame/header";
+    }
+
+    @RequestMapping("login")
+    public String login(Model model) {
+        return "/auth/login";
+    }
+
+    @RequestMapping("register")
+    public String register(Model model) {
+        return "/auth/register";
+    }
+
+    @RequestMapping("home")
+    public String home(Model model) {
+        return "/container/home";
+    }
+
+
+}
+
