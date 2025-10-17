3a54ce57d550987551ee95ef2149b51444ddadc3
KhangThinh0331 <khangthinh0331@gmail.com>
2025-10-17 23:53:10 +0700

    them chuc nang loc san pham theo loai va trang chi tiet san pham

--- src/main/java/com/poly/ubs/controller/ViewController.java
+++ src/main/java/com/poly/ubs/controller/ViewController.java
@@ -1,7 +1,9 @@
 package com.poly.ubs.controller;
 
+import com.poly.ubs.entity.Category;
 import com.poly.ubs.entity.Customer;
 import com.poly.ubs.entity.Product;
+import com.poly.ubs.repository.CategoryRepository;
 import com.poly.ubs.repository.ProductRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +11,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -22,6 +23,12 @@ import java.util.Optional;
 public class ViewController {
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+    @ModelAttribute("categories")
+    public List<Category> getCategories() {
+        return categoryRepository.findAll();
+    }
     /**
      * Hiển thị phần header
      * @param model đối tượng model để truyền dữ liệu đến view
@@ -39,11 +46,22 @@ public class ViewController {
      * @return đường dẫn đến template trang chủ
      */
     @GetMapping("home")
-    public String home(Model model, HttpSession session, @RequestParam("p") Optional<Integer> p) {
+    public String home(Model model, HttpSession session, @RequestParam("p") Optional<Integer> p, @RequestParam(value = "categoryId", required = false) String categoryId) {
         Customer loggedInUser = (Customer) session.getAttribute("loggedInUser");
         model.addAttribute("loggedInUser", loggedInUser);
         Pageable pageable = PageRequest.of(p.orElse(0), 18);
-        Page<Product> items = productRepository.findAll(pageable);
+        Page<Product> items;
+
+        if (categoryId != null && !categoryId.isEmpty()) {
+            items = productRepository.findByCategoryId(categoryId, pageable);
+        } else {
+            items = productRepository.findAll(pageable);
+        }
+
+        /**
+         * Truy xuất các phần tử trong danh sách sản phẩm
+         * Cái nào có mã loại sản phẩm gì thì gán đường dẫn image tương đương của loại sản phẩm đó
+         */
         for (Product item : items) {
             String folder = "";
             switch (item.getCategory().getId()) {
@@ -61,7 +79,31 @@ public class ViewController {
             item.setImage("products/" + folder + item.getImage());
         }
         model.addAttribute("items", items);
+        model.addAttribute("selectedCategoryId", categoryId);
         return "/container/home";
     }
 
+    @GetMapping("/product/detail/{id}")
+    public String detail(@PathVariable("id") String id, Model model) {
+        Product item = productRepository.findById(id).orElse(null);
+        String folder = "";
+        switch (item.getCategory().getId()) {
+            case "LSP01": folder = "phone/"; break;
+            case "LSP02": folder = "laptop/"; break;
+            case "LSP03": folder = "pad/"; break;
+            case "LSP04": folder = "smartwatch/"; break;
+            case "LSP05": folder = "headphone/"; break;
+            case "LSP06": folder = "keyboard/"; break;
+            case "LSP07": folder = "mouse/"; break;
+            case "LSP08": folder = "screen/"; break;
+            case "LSP09": folder = "speaker/"; break;
+            default: folder = "other/";
+        }
+        item.setImage("products/" + folder + item.getImage());
+        model.addAttribute("item", item);
+        if (item != null && item.getCategory() != null) {
+            model.addAttribute("selectedCategoryId", item.getCategory().getId());
+        }
+        return "/container/products/product-detail";
+    }
 }
