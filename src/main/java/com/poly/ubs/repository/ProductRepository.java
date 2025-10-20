6eeed08e2def4c6bbac5ea2ebdb94f5e96642e19
HyuMorreti <huyhuyhuy201107@gmail.com>
2025-10-20 17:06:05 +0700

    1705 20/10/2025

--- src/main/java/com/poly/ubs/repository/ProductRepository.java
+++ src/main/java/com/poly/ubs/repository/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.poly.ubs.repository;
 
 import com.poly.ubs.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
-
+    Page<Product> findByCategoryId(String categoryId, Pageable pageable);
 }
--- src/main/java/com/poly/ubs/service/ProductServiceImpl.java
+++ src/main/java/com/poly/ubs/service/ProductServiceImpl.java
@@ -4,7 +4,8 @@ import com.poly.ubs.entity.Product;
 import com.poly.ubs.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 /**
  * Ví dụ cài đặt dịch vụ sử dụng dịch vụ chung
  */
@@ -17,5 +18,13 @@ public class ProductServiceImpl extends GenericServiceImpl<Product, String, Prod
     @Override
     protected ProductRepository getRepository() {
         return productRepository;
+    }
+
+    public Page<Product> findByCategoryId(String categoryId, Pageable pageable) {
+        return productRepository.findByCategoryId(categoryId, pageable);
+    }
+    
+    public Page<Product> findAll(Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 }
