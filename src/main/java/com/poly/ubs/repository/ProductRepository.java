c880a166c0160005a8c2db1802b094c8f340547f
huyhuyhuy201107@gmail.com <huyhuyhuy201107@gmail.com>
2025-10-23 05:48:39 +0000

    Last commit

--- src/main/java/com/poly/ubs/repository/ProductRepository.java
+++ src/main/java/com/poly/ubs/repository/ProductRepository.java
@@ -39,4 +39,13 @@ public interface ProductRepository extends JpaRepository<Product, String> {
 //    Page<Product> findByCategory(String categoryName, Pageable pageable);
 //    Page<Product> findAll(Pageable pageable);
 //    long countByCategory(String categoryName);
+// Tìm theo tên sản phẩm
+Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
+    // Tìm theo category + tên sản phẩm
+    Page<Product> findByCategory_IdAndNameContainingIgnoreCase(String categoryId, String name, Pageable pageable);
+
+    Page<Product> findByPriceBetween(double min, double max, Pageable pageable);
+
+    Page<Product> findByPriceBetweenAndBrandId(double min, double max, String brandId, Pageable pageable);
 }
--- src/main/java/com/poly/ubs/service/BrandServiceImpl.java
+++ src/main/java/com/poly/ubs/service/BrandServiceImpl.java
@@ -5,6 +5,8 @@ import com.poly.ubs.repository.BrandRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * Cài đặt dịch vụ cho thực thể Brand sử dụng dịch vụ chung
  */
@@ -17,5 +19,8 @@ public class BrandServiceImpl extends GenericServiceImpl<Brand, String, BrandRep
     @Override
     protected BrandRepository getRepository() {
         return brandRepository;
+    }
+    public List<Brand> getBrands() {
+        return brandRepository.findAll();
     }
 }
