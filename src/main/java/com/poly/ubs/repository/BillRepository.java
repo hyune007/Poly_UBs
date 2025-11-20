94f50eebbfefd41fcf7648227db109c0ee6256bf
kohacoder <172766095+kohacoder@users.noreply.github.com>
2025-11-20 23:07:12 +0700

    Xong Quan Ly Hoa Don cua Khach Hang, cap nhat trang thai da xac nhan, da huy, xem chi tiet hoa don cua khach hang

--- src/main/java/com/poly/ubs/repository/BillRepository.java
+++ src/main/java/com/poly/ubs/repository/BillRepository.java
@@ -4,9 +4,14 @@ import com.poly.ubs.entity.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Repository cho thực thể Bill
  */
 @Repository
 public interface BillRepository extends JpaRepository<Bill, String> {
+
+    List<Bill> findByCustomerId(String customerId);
+
 }
